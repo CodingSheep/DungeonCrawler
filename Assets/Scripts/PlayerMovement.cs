12771d1ee17fff571b48432c41d444a250552dd8
Jarrod Raine <jarrodfraine@me.com>
2017-02-19 17:33:32 -0700

    Added the Testing Grounds Branch, which I hope will be where we can test out different concepts such as the Ossu Mechanic, enemies, spells, UI, etc. Also created a temporary player as wellas handled movement and created a top down camera that follows the players movements

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	//This sets a default speed for how fast our player can move in any direction
+	public float speed = 5.0f;
+
+	//Every Frame I believe
+	void Update()
+	{
+		Movement();
+	}
+
+	void Movement()
+	{
+		//Im very sure this bit is obvious, though I will explain the Time.deltaTime.
+		//Time.deltaTime is basically the time it took to finish the last frame
+		//It's used with speed to basically say "I want to move 5 meters per second, not 5 meters per frame"
+		if (Input.GetKey (KeyCode.W)) 
+		{
+			Debug.Log ("Forward!");
+			this.transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+		}
+		if (Input.GetKey (KeyCode.S)) 
+		{
+			Debug.Log ("Backward!");
+			this.transform.Translate(new Vector3(0, 0, -speed * Time.deltaTime));
+		}
+		if (Input.GetKey (KeyCode.A)) 
+		{
+			Debug.Log ("Left!");
+			this.transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+		}
+		if (Input.GetKey (KeyCode.D)) 
+		{
+			Debug.Log ("Right!");
+			this.transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+		}
+	}
+}
