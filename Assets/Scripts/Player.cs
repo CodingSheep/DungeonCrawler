8f94b2002abb2d776b80c8d998d2dea1346d0e26
nullmage <jsky.johnson@gmail.com>
2017-02-20 01:42:41 -0700

    Proposes a different movement scheme

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,36 +7,23 @@ public class Player : MonoBehaviour {
 	//This sets a default speed for how fast our player can move in any direction
 	public float speed = 5.0f;
 
-	//Every Frame I believe
-	void Update()
-	{
-		Movement();
-	}
+    private Vector3 deltamovement;
+    private GameObject gamecontroller;
 
-	void Movement()
+    //For future use.
+    void Start()
+    {
+        gamecontroller = GameObject.FindGameObjectWithTag("GameController");
+    }
+
+    //Every Frame I believe
+    void Update()
 	{
-		//Im very sure this bit is obvious, though I will explain the Time.deltaTime.
-		//Time.deltaTime is basically the time it took to finish the last frame
-		//It's used with speed to basically say "I want to move 5 meters per second, not 5 meters per frame"
-		if (Input.GetKey (KeyCode.W)) 
-		{
-			Debug.Log ("Forward!");
-			this.transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
-		}
-		if (Input.GetKey (KeyCode.S)) 
-		{
-			Debug.Log ("Backward!");
-			this.transform.Translate(new Vector3(0, 0, -speed * Time.deltaTime));
-		}
-		if (Input.GetKey (KeyCode.A)) 
-		{
-			Debug.Log ("Left!");
-			this.transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
-		}
-		if (Input.GetKey (KeyCode.D)) 
-		{
-			Debug.Log ("Right!");
-			this.transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
-		}
-	}
+        //Uses the default Unity Input object too manage player input. This allows for multiple platforms, and easy customization in the future.
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        deltamovement = transform.position + speed * (new Vector3(h, 0, v)).normalized * Time.deltaTime; //Vector of movement direction * time since last called.
+        transform.position = deltamovement;
+    }
+
 }
