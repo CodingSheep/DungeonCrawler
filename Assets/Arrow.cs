012bb4785c052459975f93c88efdd2a0fc23ace0
Joe Alsko <joal2716@colorado.edu>
2017-03-03 14:25:52 -0700

    Added arrow script to handle arrow movement and set up spell application; arrows will be instantiated with a preset spell and that spell will determine what an arrow does when hitting an enemy

---
+++ Assets/Arrow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour {
+
+	public Spell spell = new Spell(true, 1, "Basic");
+	public float speed = 40f;
+	public float lifetime = 2f;
+
+	void Start () {
+		Invoke ("DestroySelf", lifetime);
+	}
+	
+	void FixedUpdate () {
+		transform.Translate (transform.forward * speed * Time.deltaTime);
+	}
+
+	void OnCollisionEnter(Collision col) {
+		if (col.gameObject.tag == "Enemy") {
+			/*Enemy hit sequence (do damage, apply status effects, kill arrow)*/
+
+			//col.gameObject.GetComponent<EnemyHealth>().health -= spell.dmg;
+			DestroySelf();
+		} else if (col.gameObject.tag == "World") {
+			DestroySelf();
+		}
+	}
+
+	void DestroySelf() {
+		Destroy(this.gameObject);
+	}
+}
