1bbafc73ca868f5af2886558d78476d6db2806c7
nullmage <jsky.johnson@gmail.com>
2017-03-14 01:49:23 -0600

    Detaches some spell object dependancy in Arrows

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class Arrow : MonoBehaviour {
 
 	public Spell spell = new Spell(true, 1, "Basic", 1f);
-	public float speedMult = 1f;
+	public float speedMult;
 	public float lifetime = 2f;
 	public GameObject model;
 	public GameObject player = null;
     public bool Exists = false;
+
 	public void Start () {
 		player = GameObject.FindGameObjectWithTag("Player");
 		Invoke ("DestroySelf", lifetime);
-		speedMult = spell.speedMult;
+		//speedMult = spell.speedMult;
 		model = Resources.Load ("Models/" + spell.name) as GameObject;
 		if (model != null) {Debug.Log ("model loaded");}
 		this.GetComponentInChildren<MeshFilter> ().mesh = model.GetComponent<MeshFilter> ().sharedMesh;
@@ -25,13 +26,11 @@ public class Arrow : MonoBehaviour {
         if(Exists)
         {
             //player = GameObject.FindGameObjectWithTag("Player");
-            transform.Translate(Vector3.forward * speedMult * player.GetComponent<Player>().arrowSpeed * Time.deltaTime);
+            transform.Translate(Vector3.forward * speedMult * player.GetComponent<Player>().arrowSpeed);
         }
         else{
             Start();
         }
-        //Debug.Log(player.GetComponent<Player>().arrowSpeed);
-        //
     }
 
     void OnCollisionEnter(Collision col) {
