3190bc1697e4174e5630872465e3a4779e9f4957
jcbtmy <jcbtmy@gmail.com>
2017-03-19 19:07:32 -0600

    Adds q for example

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,12 +5,29 @@ using UnityEngine;
 public class GameController : MonoBehaviour {
 
 	// Use this for initialization
+	public GameObject OsuCircle;
+
 	void Start () {
 		
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
+		if (Input.GetKeyDown (KeyCode.Q)) {
+
+
+			//creates game object
+			GameObject osu_Circle= Instantiate (OsuCircle);
+
+			//displaces the circle 2 times its size above player
+			float displacement = osu_Circle.transform.localScale.x * 2;
+
+			//sets postions and parent
+			osu_Circle.transform.position = new Vector2 (Screen.width / 2, Screen.height / 2 + displacement);
+			osu_Circle.transform.SetParent (GameObject.FindGameObjectWithTag ("Canvas").transform);
+
+		}
+
 	}
 }
