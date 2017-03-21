cc880bc73bddcc7c86042c5d1b8b8b6f53a86c91
jcbtmy <jcbtmy@gmail.com>
2017-03-21 17:28:19 -0600

    Adds testworld and bigger gap between player

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -21,11 +21,12 @@ public class GameController : MonoBehaviour {
 			GameObject osu_Circle= Instantiate (OsuCircle);
 
 			//displaces the circle 2 times its size above player
-			float displacement = osu_Circle.transform.localScale.x * 2;
+			float displacement = osu_Circle.transform.localScale.x * 50;
 
 			//sets postions and parent
-			osu_Circle.transform.position = new Vector2 (Screen.width / 2, Screen.height / 2 + displacement);
 			osu_Circle.transform.SetParent (GameObject.FindGameObjectWithTag ("Canvas").transform);
+			osu_Circle.transform.position = new Vector2 (Screen.width / 2, Screen.height / 2 + displacement);
+	
 
 		}
 
