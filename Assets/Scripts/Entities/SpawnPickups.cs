cb7bea2b87d82c1b2abf305e337fadc581e9a9a8
Jarrod Raine <jarrodfraine@me.com>
2017-04-19 09:39:48 -0600

    Added Item Spawner
    Deleted the cube on the map and added an Item Spawner. It spawns the different pickups within a 20 unit radius (The variable is public so that it could be tweaked if needed). The actual item spawning is currently a WIP as some items could spawn under the map due to how Random.insideUnitSphere works.

---
+++ Assets/Scripts/Entities/SpawnPickups.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPickups : MonoBehaviour {
+
+	public float radius = 20.0f;
+	private float timer = 0.0f;
+
+	//For our Pickup list (We can add prefabs Directly from Unity)
+	public List<Pickup> PrefabList = new List<Pickup>();
+
+	void Update () {
+		timer += UnityEngine.Time.deltaTime;
+
+		//Spawns an item every 10 seconds
+		if (timer >= 10.0) {
+			//Picks a random
+			int PrefabIndex = UnityEngine.Random.Range(0, PrefabList.Count);
+			Instantiate (PrefabList[PrefabIndex], Random.insideUnitSphere * radius + transform.position, Random.rotation);
+
+			/*
+			//In case the item to spawn is below the ground. CURRENTLY A WIP
+			if (PrefabList [PrefabIndex].transform.position.y < 1)
+				PrefabList [PrefabIndex].transform.position.y = 1;
+			*/
+
+			//Reset the timer
+			timer = 0.0f;
+		}
+	}
+}
