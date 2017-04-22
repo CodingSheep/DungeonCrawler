83f0e94bf8edea67b554ffa240101d89d57d1210
Jarrod Raine <jarrodfraine@me.com>
2017-04-22 15:43:46 -0600

    Added Player Health to UI
    I can make it pretty later. Required a new script to make it work, though I could try and add the code for it to the Player Script.

---
+++ Assets/Scripts/UI/Menu/PlayerGUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerGUI : MonoBehaviour {
+
+	public Player player;
+	public GameObject UI;
+
+	private Text thing;
+	private string text;
+
+	// Use this for initialization
+	void Start () {
+		thing = UI.GetComponent <Text> ();
+		thing.text = "Health: " + player.health + "/" + player.maxHealth;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		thing.text = "Health: " + player.health + "/" + player.maxHealth;
+	}
+}
