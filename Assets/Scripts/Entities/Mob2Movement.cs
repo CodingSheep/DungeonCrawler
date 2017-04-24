baf786f51561521dad7f221b8647e6a9de88739c
Joe Alsko <joal2716@colorado.edu>
2017-04-24 13:24:51 -0600

    Fixed enemy_test scene; fixed mob2 movement to be more consistent

--- Assets/Scripts/Entities/Mob2Movement.cs
+++ Assets/Scripts/Entities/Mob2Movement.cs
@@ -1,36 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Mob2Movement : MonoBehaviour
 {
 	Transform player;
+	NavMeshAgent nav;
+	Rigidbody rb;
 
+	public float jumpRate = 2f;
+	public float jumpHeight = 300f;
 
-	// Use this for initialization
 	void Awake()
 	{
 		player = GameObject.FindGameObjectWithTag("Player").transform;
+		nav = GetComponent<NavMeshAgent>();
+		rb = GetComponent<Rigidbody> ();
+		InvokeRepeating ("Jump", 0, jumpRate);
 	}
 
-	// Update is called once per frame
 	void Update()
 	{
 		transform.LookAt(player);
-		//nav.speed = 0;
-		//nav.acceleration = 0;
 	}
 
-	IEnumerator JumpLogic()
-	{
-		float minWaitTime = 2;
-		float maxWaitTime = 4;
-
-		while(true)
-		{
-			yield return new WaitForSeconds(Random.Range(minWaitTime, maxWaitTime));
-			//Jump();
-		}
+	void Jump() {
+		rb.AddForce (new Vector3(transform.forward.x*jumpHeight/1.75f, jumpHeight, transform.forward.z*jumpHeight/1.75f));
 	}
 
 }
