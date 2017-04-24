a60ded06a155c54aa4754bb74f3024fc92f93200
Joe Alsko <joal2716@colorado.edu>
2017-04-24 14:16:42 -0600

    Added mesh for map items and added bounds; meshes for ground might make shooting uneven and navmeshing expensive, might need to flatten out ground

--- Assets/Scripts/Entities/Player.cs
+++ Assets/Scripts/Entities/Player.cs
@@ -98,7 +98,7 @@ public class Player : MonoBehaviour {
 		//Im very sure this bit is obvious, though I will explain the Time.deltaTime.
 		//Time.deltaTime is basically the time it took to finish the last frame
 		//It's used with speed to basically say "I want to move 5 meters per second, not 5 meters per frame"
-
-		transform.position = deltamovement;
+		GetComponent<CharacterController>().SimpleMove(speed * (new Vector3(h, 0, v)).normalized);
+		//transform.position = deltamovement;
 	}
 }
