7fbbd45896fd6f155dd1e15b91b21e34a9bffa12
nullmage <jsky.johnson@gmail.com>
2017-04-29 18:10:18 -0600

    Makes things spooky

--- Assets/Scripts/Entities/Mob2Movement.cs
+++ Assets/Scripts/Entities/Mob2Movement.cs
@@ -8,12 +8,13 @@ public class Mob2Movement : MonoBehaviour
 	Transform player;
 	NavMeshAgent nav;
 	Rigidbody rb;
-
+    Animator anim;
 	public float jumpRate = 2f;
 	public float jumpHeight = 1000f;
 
 	void Awake()
 	{
+        anim = GetComponent<Animator>();
 		player = GameObject.FindGameObjectWithTag("Player").transform;
 		nav = GetComponent<NavMeshAgent>();
 		rb = GetComponent<Rigidbody> ();
@@ -26,6 +27,7 @@ public class Mob2Movement : MonoBehaviour
 	}
 
 	void Jump() {
+        anim.SetTrigger("Surround Attack");
 		rb.AddForce (new Vector3(transform.forward.x*jumpHeight*1.8f, jumpHeight*0.7f, transform.forward.z*jumpHeight*1.8f));
 	}
 
